f3d05c7aae52aaebf81fe0d3ee93c07ae689dabb
Anshuman Raj <anshumanraj252@gmail.com>
2025-04-23 08:45:26 +0530

    Initial commit - Student CRUD App

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app'
+import '../style.css'
+
+import 'bulma/css/bulma.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
